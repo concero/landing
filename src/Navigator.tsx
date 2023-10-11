1db0fdd5eface7edc96f46794cea324b74740ead
lufaque <95128636+lufaque@users.noreply.github.com>
2023-10-11 19:09:31 +0300

    fixes

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,16 +1,6 @@
 import { FC } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AppScreen } from './components/screens/AppScreen/AppScreen'
 import { HomePage } from './components/pages/HomePage'
 
 export interface NavigatorProps {}
 
-export const Navigator: FC<NavigatorProps> = () => (
-  <BrowserRouter>
-    <AppScreen>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </AppScreen>
-  </BrowserRouter>
-)
+export const Navigator: FC<NavigatorProps> = () => <HomePage />
