3756ac6f3f627cc394d3d54e3f338a0d96a795ae
Oleg Kirillovich <olegkron@ya.ru>
2023-10-23 22:43:39 +0100

    rango img

--- src/components/blocks/PartnersBlock/items.ts
+++ src/components/blocks/PartnersBlock/items.ts
@@ -2,7 +2,7 @@ import lifiLogo from '../../../assets/images/Partner/lifi.svg'
 import enso from '../../../assets/images/Partner/enso.svg'
 import insurace from '../../../assets/images/Partner/insurace.svg'
 import aave from '../../../assets/images/Partner/aave.svg'
-import rango from '../../../assets/images/Partner/rango.svg'
+import rango from '../../../assets/images/Partner/Rango.jpg'
 import comingSoon from '../../../assets/images/Partner/comingSoon.svg'
 
 export const items = [
