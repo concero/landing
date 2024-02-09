19f58ff6afd1a9730ecb90921543d4fc7452d8df
lufaque <95128636+lufaque@users.noreply.github.com>
2024-02-09 17:15:53 +0300

    fix: main screen button click, main screen figure position

--- src/components/screens/MainScreen/MainScreen.tsx
+++ src/components/screens/MainScreen/MainScreen.tsx
@@ -1,19 +1,10 @@
 import classNames from './MainScreen.module.pcss'
 import mainPageFigure from '../../../assets/images/mainScreen/mainPageFigure.png'
-import { useMediaQuery } from '../../../hooks/useMediaQueryHook'
 import { Button } from '../../layout/Button/Button'
 import { IconArrowUpRight } from '@tabler/icons-react'
 import { Header } from '../../layout/Header/Header'
 
 export const MainScreen = () => {
-  const isMobile = useMediaQuery('mobile')
-  const slideUpLeft = {
-    from: { x: 50, y: 50 },
-    to: { x: 0, y: 0 },
-  }
-  // const [ref, springs] = useInView(() => fadeInUp)
-  // const [imgRef, imgSprings] = useInView(() => slideUpLeft)
-
   return (
     <div className={`screenContainer ${classNames.container}`}>
       <Header />
@@ -41,11 +32,6 @@ export const MainScreen = () => {
           <div className={classNames.imgContainer}>
             <img src={mainPageFigure} />
           </div>
-          {isMobile ? (
-            <div className={classNames.imgContainer2}>
-              <img src={mainPageFigure} />
-            </div>
-          ) : null}
         </div>
       </div>
     </div>
