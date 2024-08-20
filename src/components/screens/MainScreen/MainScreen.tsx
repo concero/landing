1c273c6c7aa8191cc0dc1ee5e2334aa04277eb70
shtolle <eva.semenov1@gmail.com>
2024-08-20 15:28:46 +0300

    Desktop done

--- src/components/screens/MainScreen/MainScreen.tsx
+++ src/components/screens/MainScreen/MainScreen.tsx
@@ -13,11 +13,12 @@ export const MainScreen = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
 
+  const handleClick = (link) => {
+    window.open(link, '_blank')
+  }
   return (
     <div className={`screenContainer ${classNames.container}`}>
-      {/* <div className="cf-turnstile" data-sitekey="0x4AAAAAAAWGficfp02MJ5Oe" data-callback="javascriptCallback"></div>
-      <div id="captcha" /> */}
-      {/* <div className={classNames.bgContainer}> */}
+      {/* Existing content */}
       <div className={classNames.blank} />
       <div className={classNames.titleContainer}>
         <h1 className={classNames.header}>
@@ -27,13 +28,25 @@ export const MainScreen = () => {
         <div className={classNames.captionContainer}>
           <h3 className={classNames.caption}>*And we mean Fully Decentralised</h3>
           <div className={classNames.socialIconsContainer}>
-            <div className={classNames.socialIconContainer} style={{ backgroundColor: '#475467' }}>
+            <div
+              className={classNames.socialIconContainer}
+              style={{ backgroundColor: '#475467' }}
+              onClick={() => handleClick('https://x.com/concero_io')}
+            >
               <XIcon width={32} height={32} fill="white" />
             </div>
-            <div className={classNames.socialIconContainer} style={{ backgroundColor: '#5865F2' }}>
+            <div
+              className={classNames.socialIconContainer}
+              style={{ backgroundColor: '#5865F2' }}
+              onClick={() => handleClick('https://discord.gg/nBMAdxYTZK')}
+            >
               <DiscordIcon width={32} height={32} fill="white" />
             </div>
-            <div className={classNames.socialIconContainer} style={{ backgroundColor: '#475467' }}>
+            <div
+              className={classNames.socialIconContainer}
+              style={{ backgroundColor: '#475467' }}
+              onClick={() => handleClick('https://medium.com/@concero')}
+            >
               <MediumIcon width={32} height={32} fill="white" />
             </div>
           </div>
@@ -41,7 +54,11 @@ export const MainScreen = () => {
       </div>
       <img src="src/assets/images/landing/Desktop.png" alt="Landing Image" />
 
-      {/* </div> */}
+      {/* Coming Soon Message */}
+      <div className={classNames.comingSoon}>
+        <p>Website is coming soon, stay in touch</p>
+      </div>
+
       <ContactModal
         show={isModalVisible}
         setShow={setIsModalVisible}
